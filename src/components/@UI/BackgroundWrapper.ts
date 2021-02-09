68c8a0e5185c2d8b73e8b701fbe31c3add90f0c8
Fabio Benedetti <fabio.benedetti@joyn.de>
2021-02-09 22:13:42 +0100

    fix: build

--- src/components/@UI/BackgroundWrapper.ts
+++
@@ -1,7 +0,0 @@
-import tw from "twin.macro";
-
-export const BackgroundBlueWrapper = tw.div`bg-gradient-to-b from-blue-200 to-white`;
-
-export const BackgroundPinkWrapper = tw.div`bg-gradient-to-b from-pink-200 to-white`;
-
-export const BackgroundYellowWrapper = tw.div`bg-gradient-to-b from-yellow-200 to-white`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,6 @@ import { graphCmsRequest } from "@/graphql/graphcms";
 import { GetStaticProps } from "next";
 import React from "react";
 
-import { Hero } from "../components/Home/1Hero";
-import { Goals } from "../components/Home/2Goals";
-import { Map } from "../components/Home/3Map";
-import { Social } from "../components/Home/4Social";
-import { Sponsors } from "../components/Home/5Sponsors";
-import { withGrommetTheme } from "../components/Layout/withLayout";
-
 export const getStaticProps: GetStaticProps = async () => {
   const data = await graphCmsRequest(HomePageDocument);
   return {
