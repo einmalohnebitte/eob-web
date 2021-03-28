3e020d05d5bf55d896f11376b470e8c4efa8a9cd
Fabio Benedetti <fabio.benedetti@joyn.de>
2021-03-28 12:32:46 +0200

    feat: matomo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,6 +33,22 @@ export default class MyDocument extends Document<{
 
           <link rel="icon" href="/favicon.ico" type="image/ico" />
           {this.props.styleTags}
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+  var _paq = window._paq = window._paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//${process.env.NEXT_PUBLIC_MATOMO_URL}/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })(); `,
+            }}
+          />
         </Head>
         <body>
           <Main />
