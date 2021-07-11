f5cea208c97b47a9380f3ba982d2c2e349768262
Akansha-star <sendtoakansha@gmail.com>
2021-07-11 23:43:45 +0200

    3 Sections made responsive for mobile.

--- src/components/@UI/Section.tsx
+++ src/components/@UI/Section.tsx
@@ -1,11 +1,23 @@
 import styled from "styled-components";
 import tw from "twin.macro";
+import { MQ_DESKTOP, MQ_MOBILE } from "@/constants/MediaQueries";
 
 export const Section = tw.section`p-1 md:p-8 max-w-screen-lg mx-auto `;
 
+export const HomeSection = {
+  Section: styled(Section)` 
+    @media ${MQ_MOBILE} {
+      ${tw`flex flex-col`}
+    }
+    @media ${MQ_DESKTOP} {
+      ${tw`flex flex-row`}
+    }
+  `,
+};
+
 export const SplitSection = {
   Section: styled(Section)`
-    ${tw`flex flex-col md:flex-row items-center`}
+    ${tw`flex flex-col items-center md:flex-row`}
     div:first-child {
       ${tw`md:mr-8`}
     }
