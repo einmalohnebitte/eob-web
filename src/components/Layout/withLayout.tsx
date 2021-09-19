d215b1234cf8918a2fa2f357aaa921234895d2cc
Fabio Benedetti <fabio.benedetti@joyn.de>
2021-09-19 09:59:26 +0200

    fix: linter

--- src/components/Layout/withLayout.tsx
+++ src/components/Layout/withLayout.tsx
@@ -25,34 +25,38 @@ const LinkCookie = styled(Link)`
 
 const queryClient = new QueryClient();
 
-export const withLayout = () => (Comp: any) => (props: any) => {
-  const intl = useTranslations(props.translations);
-  const router = useRouter();
-  return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <GlobalStyle />
-        {/* <SEO /> */}
-        <Header />
+export const withLayout =
+  () =>
+  (Comp: any) =>
+  // eslint-disable-next-line react/display-name
+  (props: any): React.ReactElement => {
+    const intl = useTranslations(props.translations);
+    const router = useRouter();
+    return (
+      <>
+        <QueryClientProvider client={queryClient}>
+          <GlobalStyle />
+          {/* <SEO /> */}
+          <Header />
 
-        <Layout>
-          <Comp {...props} />
-        </Layout>
-        <AppFooter />
-        <CookieConsent
-          buttonStyle={{ background: "white", borderRadius: "10px" }}
-          style={{ background: "rgba(0,0,0,0.75)", zIndex: 999999 }}
-        >
-          {intl("COOKIE_TEXT")}{" "}
-          <LinkCookie href="/datenschutz">
-            <a>{intl("COOKIE_LINK")}</a>
-          </LinkCookie>
-        </CookieConsent>
-        <FaQuestion
-          onClick={() => router.push("/faq")}
-          css={tw`w-8 h-8 fixed text-pink-500 bottom-2.5 right-2.5 cursor-pointer hover:w-10 hover:h-10`}
-        />
-      </QueryClientProvider>
-    </>
-  );
-};
+          <Layout>
+            <Comp {...props} />
+          </Layout>
+          <AppFooter />
+          <CookieConsent
+            buttonStyle={{ background: "white", borderRadius: "10px" }}
+            style={{ background: "rgba(0,0,0,0.75)", zIndex: 999999 }}
+          >
+            {intl("COOKIE_TEXT")}{" "}
+            <LinkCookie href="/datenschutz">
+              <a>{intl("COOKIE_LINK")}</a>
+            </LinkCookie>
+          </CookieConsent>
+          <FaQuestion
+            onClick={() => router.push("/faq")}
+            css={tw`w-8 h-8 fixed text-pink-500 bottom-2.5 right-2.5 cursor-pointer hover:w-10 hover:h-10`}
+          />
+        </QueryClientProvider>
+      </>
+    );
+  };
