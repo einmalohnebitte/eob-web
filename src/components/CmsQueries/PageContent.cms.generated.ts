743292d2bf10abc55dbe2546f9ca93a5d6e8f7f0
Fabio Benedetti <fabio.benedetti+github@gmail.com>
2021-11-03 19:41:09 +0100

    feat: update codegen

--- src/components/CmsQueries/PageContent.cms.generated.ts
+++ src/components/CmsQueries/PageContent.cms.generated.ts
@@ -1,6 +1,5 @@
 import * as Types from '../../server/generated/graphql';
 
-import { TranslationsFragment } from '../../hooks/useTranslations/Translations.cms.generated';
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 import { TranslationsFragmentDoc } from '../../hooks/useTranslations/Translations.cms.generated';
 export type PageContentQueryVariables = Types.Exact<{
@@ -9,20 +8,7 @@ export type PageContentQueryVariables = Types.Exact<{
 }>;
 
 
-export type PageContentQuery = (
-  { __typename?: 'Query' }
-  & { pages: Array<(
-    { __typename?: 'Page' }
-    & Pick<Types.Page, 'title' | 'metaDescription' | 'metaKeywords' | 'slug'>
-    & { content?: Types.Maybe<(
-      { __typename?: 'RichText' }
-      & Pick<Types.RichText, 'html'>
-    )> }
-  )>, translations: Array<(
-    { __typename?: 'Translation' }
-    & TranslationsFragment
-  )> }
-);
+export type PageContentQuery = { __typename?: 'Query', pages: Array<{ __typename?: 'Page', title?: string | null | undefined, metaDescription?: string | null | undefined, metaKeywords?: string | null | undefined, slug?: string | null | undefined, content?: { __typename?: 'RichText', html: string } | null | undefined }>, translations: Array<{ __typename?: 'Translation', locale: Types.Locale, key: string, value?: string | null | undefined }> };
 
 
 export const PageContentDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"PageContent"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"page"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}},"defaultValue":{"kind":"StringValue","value":"home","block":false}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"locale"}},"type":{"kind":"NonNullType","type":{"kind":"ListType","type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"Locale"}}}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"pages"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"where"},"value":{"kind":"ObjectValue","fields":[{"kind":"ObjectField","name":{"kind":"Name","value":"slug"},"value":{"kind":"Variable","name":{"kind":"Name","value":"page"}}}]}},{"kind":"Argument","name":{"kind":"Name","value":"locales"},"value":{"kind":"Variable","name":{"kind":"Name","value":"locale"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"title"}},{"kind":"Field","name":{"kind":"Name","value":"content"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"html"}}]}},{"kind":"Field","name":{"kind":"Name","value":"metaDescription"}},{"kind":"Field","name":{"kind":"Name","value":"metaKeywords"}},{"kind":"Field","name":{"kind":"Name","value":"slug"}}]}},{"kind":"Field","name":{"kind":"Name","value":"translations"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"locales"},"value":{"kind":"Variable","name":{"kind":"Name","value":"locale"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"FragmentSpread","name":{"kind":"Name","value":"Translations"}}]}}]}},...TranslationsFragmentDoc.definitions]} as unknown as DocumentNode<PageContentQuery, PageContentQueryVariables>;
--- src/components/CmsQueries/ShopsNumber.cms.generated.ts
+++ src/components/CmsQueries/ShopsNumber.cms.generated.ts
@@ -4,19 +4,7 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
 export type ShopsNumberQueryVariables = Types.Exact<{ [key: string]: never; }>;
 
 
-export type ShopsNumberQuery = (
-  { __typename?: 'Query' }
-  & { shopsConnection: (
-    { __typename?: 'ShopConnection' }
-    & { aggregate: (
-      { __typename?: 'Aggregate' }
-      & Pick<Types.Aggregate, 'count'>
-    ) }
-  ), kpis: Array<(
-    { __typename?: 'Kpi' }
-    & Pick<Types.Kpi, 'text' | 'value'>
-  )> }
-);
+export type ShopsNumberQuery = { __typename?: 'Query', shopsConnection: { __typename?: 'ShopConnection', aggregate: { __typename?: 'Aggregate', count: number } }, kpis: Array<{ __typename?: 'Kpi', text?: string | null | undefined, value?: number | null | undefined }> };
 
 
 export const ShopsNumberDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"ShopsNumber"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"shopsConnection"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"stage"},"value":{"kind":"EnumValue","value":"PUBLISHED"}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"aggregate"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"count"}}]}}]}},{"kind":"Field","name":{"kind":"Name","value":"kpis"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"text"}},{"kind":"Field","name":{"kind":"Name","value":"value"}}]}}]}}]} as unknown as DocumentNode<ShopsNumberQuery, ShopsNumberQueryVariables>;
