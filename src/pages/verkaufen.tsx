7eb95e1259d8cf50ca345b52c0092150df51d105
Fabio Benedetti <fabio.benedetti+github@gmail.com>
2021-11-03 19:56:39 +0100

    feat: verkaufen list

--- src/pages/verkaufen.tsx
+++ src/pages/verkaufen.tsx
@@ -58,8 +58,26 @@ const Home: React.FC<PageSectionsQuery> = ({ pages, pageSections }) => {
           <div
             css={`
               ol {
-                display: flex;
-                flex-direction: row;
+                padding: 2rem;
+                list-style: none;
+                counter-reset: my-awesome-counter;
+                li::before {
+                  content: counter(my-awesome-counter);
+                  font-weight: bold;
+                  font-size: 3rem;
+                  margin-right: 1rem;
+                  font-family: "Lemonism-Regular", serif;
+                  line-height: 1.5;
+                }
+                li {
+                  counter-increment: my-awesome-counter;
+                  display: flex;
+                  flex-direction: row;
+                  align-items: center;
+                }
+                div {
+                  display: flex;
+                }
               }
 
               ${tw`py-4 pr-4`}
@@ -75,6 +93,7 @@ const Home: React.FC<PageSectionsQuery> = ({ pages, pageSections }) => {
             css={`
               ${tw`py-4 `}
               ul {
+                padding: 2rem;
                 text-indent: 10px;
                 list-style: disc inside;
                 div {
