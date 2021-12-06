af43d7649bb8b36cbac5521703cf4569345e93e8
Fabio Benedetti <fabio.benedetti+github@gmail.com>
2021-12-06 21:36:41 +0100

    fix: presse

--- src/components/@UI/Texts.tsx
+++ src/components/@UI/Texts.tsx
@@ -7,7 +7,7 @@ export const H2 = styled.h2`
   line-height: 3rem;
   ${tw`font-lemonism`}
 `;
-export const H3 = tw.h3`font-lemonism text-4xl leading-6 `;
+export const H3 = tw.h3`font-lemonism text-4xl leading-8 `;
 export const H4 = tw.h3`font-lemonism text-xl leading-6 `;
 
 export const PFont = `text-xl leading-8 font-gt`;
