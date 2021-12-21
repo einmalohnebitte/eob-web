9dde088faa020d820cca63ecacb174c6502505e0
Fabio Benedetti <fabio.benedetti+github@gmail.com>
2021-12-21 21:52:18 +0100

    fix: tsc

--- src/components/@UI/BackgroundWrapper.tsx
+++ src/components/@UI/BackgroundWrapper.tsx
@@ -1,13 +1,29 @@
+/* eslint-disable react/display-name */
 import React from "react";
 
-export const BackgroundBlueWrapper: React.FC = ({ children }) => (
-  <div className="bg-gradient-to-b from-blue-200 to-white">{children}</div>
-);
+export const BackgroundBlueWrapper = React.forwardRef<
+  HTMLDivElement,
+  React.PropsWithChildren<{}>
+>(({ children }, ref) => (
+  <div ref={ref} className="bg-gradient-to-b from-blue-200 to-white">
+    {children}
+  </div>
+));
 
-export const BackgroundPinkWrapper: React.FC = ({ children }) => (
-  <div className="bg-gradient-to-b from-pink-200 to-white">{children}</div>
-);
+export const BackgroundPinkWrapper = React.forwardRef<
+  HTMLDivElement,
+  React.PropsWithChildren<{}>
+>(({ children }, ref) => (
+  <div ref={ref} className="bg-gradient-to-b from-pink-200 to-white">
+    {children}
+  </div>
+));
 
-export const BackgroundYellowWrapper: React.FC = ({ children }) => (
-  <div className="bg-gradient-to-b from-yellow-200 to-white">{children}</div>
-);
+export const BackgroundYellowWrapper = React.forwardRef<
+  HTMLDivElement,
+  React.PropsWithChildren<{}>
+>(({ children }, ref) => (
+  <div ref={ref} className="bg-gradient-to-b from-yellow-200 to-white">
+    {children}
+  </div>
+));
