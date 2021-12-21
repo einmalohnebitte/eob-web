ee8f73f3ab2befb6dce6a6b79fbe48e6d14aeb8c
Fabio Benedetti <fabio.benedetti+github@gmail.com>
2021-12-21 21:45:34 +0100

    feat: card

--- src/components/@UI/Card.tsx
+++ src/components/@UI/Card.tsx
@@ -1,22 +1,22 @@
+import classNames from "classnames";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
-import tw, { TwStyle } from "twin.macro";
 import { H2 } from "./Texts";
 
-export const getBorderColor = (color?: "pink" | "blue" | "yellow"): TwStyle =>
+export const getBorderColor = (color?: "pink" | "blue" | "yellow"): string =>
   color === "blue"
-    ? tw`border-blue-500`
+    ? `border-blue-500`
     : color === "pink"
-    ? tw`border-pink-500`
-    : tw`border-yellow-600`;
+    ? `border-pink-500`
+    : `border-yellow-600`;
 
-export const getTextColor = (color?: "pink" | "blue" | "yellow"): TwStyle =>
+export const getTextColor = (color?: "pink" | "blue" | "yellow"): string =>
   color === "blue"
-    ? tw`text-blue-500`
+    ? `text-blue-500`
     : color === "pink"
-    ? tw`text-pink-500`
-    : tw`text-yellow-600`;
+    ? `text-pink-500`
+    : `text-yellow-600`;
 
 export const Card: React.FC<{
   img?: string;
@@ -58,35 +58,38 @@ export const Card: React.FC<{
           router.push(linkTo);
         }
       }}
-      css={[
-        tw`flex flex-col content-between cursor-pointer transform scale-100 sm:hover:scale-105 sm:max-w-md mx-auto bg-white border-l-4  border-b-8 border-r-4 border-t-2  mb-4 overflow-hidden sm:m-4   `,
-        getBorderColor(color),
-      ]}
+      className={classNames(
+        `flex flex-col content-between cursor-pointer transform scale-100 sm:hover:scale-105 sm:max-w-md mx-auto bg-white border-l-4  border-b-8 border-r-4 border-t-2  mb-4 overflow-hidden sm:m-4   `,
+        getBorderColor(color)
+      )}
     >
       {children}
       {img && (
         <img
-          css={[tw`h-52 `, !resize && tw` w-full object-cover `]}
+          className={classNames("h-52", !resize && "w-full object-cover")}
           src={img}
-          alt="Man looking at item at a store"
+          alt=""
         />
       )}
-      <div css={tw`p-4`}>
+      <div className="p-4">
         <H2>{title}</H2>
-        {message && <p css={tw`mt-2 mb-8 text-gray-500`}>{message}</p>}
+        {message && <p className="mt-2 mb-8 text-gray-500">{message}</p>}
         {messageHtml && (
           <div
-            css={tw`mt-2 text-gray-500`}
+            className="mt-2 text-gray-500"
             dangerouslySetInnerHTML={{ __html: messageHtml }}
           />
         )}
       </div>
       <div
-        css={[getTextColor(color), tw`flex-grow flex justify-end flex-col m-2`]}
+        className={classNames(
+          "flex-grow flex justify-end flex-col m-2",
+          getTextColor(color)
+        )}
       >
         {linkTo && (
           <Link href={linkTo ?? ""}>
-            <a css={tw`hover:underline`}>{linkTitle}</a>
+            <a className="hover:underline">{linkTitle}</a>
           </Link>
         )}
         {subtitle && <i>{subtitle}</i>}
