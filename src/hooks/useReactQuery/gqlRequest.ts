07b21184ebbd51e7f88a0f7024248cce6ab88655
Fabio Benedetti <fabio.benedetti@vercel.com>
2023-02-13 21:01:29 +0100

    fix types

--- src/hooks/useReactQuery/gqlRequest.ts
+++ src/hooks/useReactQuery/gqlRequest.ts
@@ -5,4 +5,8 @@ export const gqlRequest = <TData, TVariables>(
   document: TypedDocumentNode<TData, TVariables>,
   variables?: TVariables,
   url = `/api/graphql`
-) => new GraphQLClient(url).request<TData, TVariables>(document, variables);
+) =>
+  (new GraphQLClient(url) as any).request(
+    document,
+    variables
+  ) as Promise<TData>;
