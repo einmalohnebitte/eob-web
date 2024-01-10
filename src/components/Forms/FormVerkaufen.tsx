ae38c6104f1d49e2455ccb784d4bc2ba2c1201f1
Markus Jochim <contact-markus@mensing-jochim.de>
2024-01-10 22:18:54 +0100

    Revert "Abwesenheitsnotiz Winterpause"
    This reverts commit e0db4d6005aaa193250ecc8e904ae9f4277426c2.

--- src/components/Forms/FormVerkaufen.tsx
+++ src/components/Forms/FormVerkaufen.tsx
@@ -85,21 +85,7 @@ export const FormVerkaufen: React.FC = () => {
         send({
           email,
           subject: `[Verkaufen] ${shop} ${firstName} ${lastName}`, // Subject line
-          html: `
-          Hallo,<br>
-          <br>
-          vielen Dank für deine Nachricht.<br>
-          <br>
-          Das Team von Einmal ohne, bitte macht eine kleine Winterpause. Wir sind ab dem 8. Januar wieder am Start und melden uns schnellstmöglich bei dir.<br>
-          <br>
-          Bis dahin wünschen wir Dir eine schöne Weihnachtszeit und ein entspanntes Jahresende!<br>
-          <br>
-          Liebe Grüße<br>
-          <br>
-          Das Team von Einmal ohne, bitte<br>
-          <br>
-          <br>
-          <h1>${shop} (${firstName} ${lastName})</h1><p>Email: ${email} </p><p>Address: ${address}, ${postCode}, ${town} </p><p>Stickers: ${sticker} </p><p>Message: ${message} </p>`,
+          html: `<h1>${shop} (${firstName} ${lastName})</h1><p>Email: ${email} </p><p>Address: ${address}, ${postCode}, ${town} </p><p>Stickers: ${sticker} </p><p>Message: ${message} </p>`,
         });
       }}
     >
@@ -160,14 +146,6 @@ export const FormVerkaufen: React.FC = () => {
       </div>
 
       <FieldArea field="message" label={intl("FORM_MESSAGE")} />
-
-      <div className="m-2">
-        <span className="mt-6 font-gt text-gray-700">
-          Das Team von Einmal ohne, bitte macht eine kleine Winterpause. Wir sind ab dem 8. Januar wieder am Start und beantworten Nachrichten dann schnellstmöglich.<br />
-          Bis dahin wünschen wir Dir eine schöne Weihnachtszeit und ein entspanntes Jahresende!
-        </span>
-      </div>
-
     </FormBase>
   );
 };
